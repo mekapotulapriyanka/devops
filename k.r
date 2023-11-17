5d239ee6fe0a433a140331b4d6579175731175ac
mekapotulapriyanka <151113604+mekapotulapriyanka@users.noreply.github.com>
2023-11-17 10:29:34 +0530

    factorial

--- k.r
+++ k.r
@@ -1,5 +1,10 @@
-a<-50
-b<-90
-typeof(a)
-c<-93
-typeof(c)
+n=int(input("enter a number"))
+factorial=1
+if n<0:
+    print("not a factorial")
+elif n==0:
+    print("factorial is 1")
+else:
+    for i in range(1,n+1):
+         factorial=factorial*i
+    print("the factorial is",factorial)
